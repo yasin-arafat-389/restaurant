50c5aa195b68faa3efa96721b7b51a48f2a8e7d6
Yasin <yasin.arafat.rwd@gmail.com>
2023-01-31 10:02:56 +0600

    component tree

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default Header;
