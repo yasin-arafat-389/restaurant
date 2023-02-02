82063c0ea3278a04475342b94c3e0a9b5a8b44e7
Yasin <yasin.arafat.rwd@gmail.com>
2023-02-02 20:15:55 +0600

    Designing menu items

--- src/Components/Body/MenuItem.js
+++ src/Components/Body/MenuItem.js
@@ -1,8 +1,28 @@
 import React from "react";
+import "./stylings.css";
 
 const MenuItem = (props) => {
-  console.log(props);
-  return <div></div>;
+  return (
+    <div className="container">
+      <div class="card">
+        <img
+          src={props.dish.image}
+          class="card-img-top"
+          alt={props.dish.name}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{props.dish.name}</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuItem;
